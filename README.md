71ca11d1ce5a30b5cc17a685f61af84b7ee253ce
rmichellemorton@gmail.com <lfry706@gmail.com>
2020-01-29 15:03:09 -0500

    work

--- README.md
+++ README.md
@@ -6,10 +6,11 @@ gsdfgsdfgdfs
 
 Here is something new
 
-# Code Boot Camp 20/20 :boom:
+## Code Boot Camp 20/20 boom
 
 _This is a very good class that I am going to get all I can from it._
 `<The **Future** Is _Now_!>
+
 1. Coding
 2. Cyber
 3. Information Systems
---
+++ firstdaDatatype.js
@@ -0,0 +1,55 @@
+// var haveMoney;
+// var fastFood;
+// var type;
+// var restaurant;
+
+// haveMoney = true;
+// fastfood = false;
+// homeMade = "no";
+// type = "burger";
+// restaurant = "deli";
+
+// if(haveMoney){
+// if(fastFood){
+//   if(type === "burger"){
+//       console.log("5guys");
+//   } else if(type === "wings"){
+//       console.log("aTown");
+//     } else if(type === "salad"); {
+
+// }
+
+//     } else if(homeMade === "yes"){
+//      console.log("goHome");
+//     } else if(homeMade === "don't like it"){
+//     console.log("ask a friend"); {
+
+//     } else if()
+
+//Declaring Variables
+var firstName;
+var lastName;
+
+var num1 = 100;
+var selected = true;
+
+// Assigning values to variables
+firstName = "John";
+lastName = "Smith";
+
+var fullName = firstName + " " + lastName;
+
+//Output
+console.log(fullName);
+console.log(num1);
+console.log(selected);
+
+var age = 27;
+var age2 = "28";
+var totalage = age * age2;
+var bonus = 2 + true;
+
+console.log(totalage);
+console.log(2 + true);
+
+// Bonus returned: 3
