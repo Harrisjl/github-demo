beed2a63f349f2bb78458c513020973d9e02d31a
harrisjl <jimmyl_harris@outlook.com>
2020-02-17 16:09:47 -0500

    add

--- homework/mocha/domelements.js
+++ homework/mocha/domelements.js
@@ -2,6 +2,7 @@ var el_up = document.getElementById("HSA_UP");
 var el_down = document.getElementById("HSA_DOWN");
 var heading = document.getElementById("h1");
 el_up.innerHTML = "Click on the button to " + "change the style attribute";
+var x = document.getElementById("item1").nextElementSibling.innerHTML;
 
 function hsa_Run() {
   heading.style["color"] = "white";
